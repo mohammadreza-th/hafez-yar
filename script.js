799f342e1beb85bc3b31fdd74c36c9b241ba0ebb
Mohammdreza.TH <mth.cb1446@gmail.com>
2023-07-26 22:42:20 +0330

    calculation bugs fixed!

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 // بسم الله الرحمن الرحیم
 
-let lines = document.getElementById('lines');
+let lines = document.getElementById("lines");
 let pages = document.getElementById("pages");
-let pagesDuration = document.getElementById("pages__duration")
-let linesDuration = document.getElementById("lines__duration")
-
+let pagesDuration = document.getElementById("pages__duration");
+let linesDuration = document.getElementById("lines__duration");
 
 const allPages = 604;
 const allLines = 602 * 15 + 8 * 2;
 
 function linesToDays(hafezLines) {
-  if (hafezLines === undefined|| 0) {
-return "(لطفا در وارد کردن تعداد سطر ها دقت فرمایید)"
+  if (hafezLines == 0 || undefined) {
+    return "(لطفا در وارد کردن تعداد صفحات دقت فرمایید)";
+  } else {
+    let allDays = allLines / hafezLines;
+    let years = Math.floor(allDays / 365);
+    let extraDays = allDays % 365;
+    let monthes = Math.floor(extraDays / 30);
+    let days = Math.floor(extraDays % 3) + 1;
+    return `${years !== 0 ? `${years} سال و` : ""} 
+    ${monthes !== 0 ? `${monthes} ماه و` : ""} 
+    ${days !== 0 ? `${days} روز` : ""}`;
   }
-  let allDays = allLines / hafezLines;
-  let years = Math.floor(allDays/365)
-  let extraDays = allDays%365
-  let monthes = Math.floor(extraDays/30)
-  let days = Math.floor(extraDays%3) + 1
-  return `${years!==0 ? `${years} سال`:true} و
-  ${monthes!==0 ? `${monthes} ماه`:true} و 
-  ${days!==0 ? `${days} روز`:true}`;
 }
 function pagesToDays(hafezPages) {
-  if (hafezPages === undefined|| 0) {
-    return "(لطفا در وارد کردن تعداد صفحات دقت فرمایید)"
-      }
-  let allDays = allPages / hafezPages;
-  let years = Math.floor(allDays/365)
-  let extraDays = allDays%365
-  let monthes = Math.floor(extraDays%30)
-  let days = Math.floor(extraDays%3) + 1
-  return `${years!==0 ? `${years} سال`:true} و
-  ${monthes!==0 ? `${monthes} ماه`:true} و 
-  ${days!==0 ? `${days} روز`:true}`;
+  if (hafezPages == 0 || undefined) {
+    return "(لطفا در وارد کردن تعداد صفحات دقت فرمایید)";
+  } else {
+    let allDays = allPages / hafezPages;
+    let years = Math.floor(allDays / 365);
+    let extraDays = allDays % 365;
+    let monthes = Math.floor(extraDays / 30);
+    let days = Math.floor(extraDays % 3) + 1;
+    return `${years !== 0 ? `${years} سال و` : ""} 
+    ${monthes !== 0 ? `${monthes} ماه و` : ""} 
+    ${days !== 0 ? `${days} روز` : ""}`;
+  }
 }
 
-
-lines.addEventListener("keyup", ()=>{
-linesDuration.innerHTML = linesToDays(lines.value)}
-)
-pages.addEventListener("keyup", ()=>{
-pagesDuration.innerHTML = pagesToDays(pages.value)}
-)
-console.log(Math.floor(800%365));
+lines.addEventListener("keyup", () => {
+  linesDuration.innerHTML = linesToDays(lines.value);
+});
+pages.addEventListener("keyup", () => {
+  pagesDuration.innerHTML = pagesToDays(pages.value);
+});
+console.log(Math.floor(800 % 365));
